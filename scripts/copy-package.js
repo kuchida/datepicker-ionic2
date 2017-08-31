df9450541927bc005542cca4c1fa1ecc41813be0
Kazutaka Uchida <uchida@kadinche.com>
2017-08-31 12:42:20 +0900

    change scrpit

--- scripts/copy-package.js
+++ scripts/copy-package.js
@@ -10,5 +10,13 @@ delete packageJson.devDependencies;
 
 console.log('Hi');
 console.log(process.cwd());
-fs.mkdirSync('./dist');
+//fs.mkdirSync('./dist');
 fs.writeFile('./dist/package.json', JSON.stringify(packageJson, null, 2));
+fs.readdir('./dist', function(err, files){
+    if (err) throw err;
+    var fileList = [];
+    files.forEach(function (file) {
+        fileList.push(file);
+    });
+    console.log(fileList);
+});
